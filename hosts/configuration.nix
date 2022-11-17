02e3d8ec211afc8f6ed4839e61e66ae9b511f875
Gabriel Prost <gabrielprost@hotmail.com>
2022-11-17 17:37:11 -0300

    update

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -1,23 +1,13 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { user, config, lib, pkgs, ... }:
 
 {
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.${user} = {
     isNormalUser = true;
     description = user;
     home = "/home/${user}";
     extraGroups = [ "networkmanager" "wheel" "video" "audio" ];
   };
-
-  # # Bootloader.
-  # boot.loader.systemd-boot.enable = true;
-  # boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   nixpkgs.config.allowUnfree = true;
 
