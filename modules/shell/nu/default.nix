8f0ac2b48019ce941107312ce41627fe18f093bd
Gabriel Prost <gabrielprost@hotmail.com>
2023-03-26 14:24:17 -0300

    add starship to nu

--- modules/shell/nu/default.nix
+++ modules/shell/nu/default.nix
@@ -1,7 +1,7 @@
 { makeDefault ? false }:
 { pkgs, user, ... }:
 rec {
-  environment.systemPackages =  [ pkgs.nushell ];
+  environment.systemPackages =  [ pkgs.nushell pkgs.starship ];
   environment.shells = if makeDefault then [ pkgs.nushell ] else [];
   users.users.${user}.shell = if makeDefault then pkgs.nushell else users.users.${user}.shell;
 }
