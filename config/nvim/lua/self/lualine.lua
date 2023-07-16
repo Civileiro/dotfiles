00f051594bb2587cf47ec305ef40a515693be387
Gabriel Prost <gabrielprost@hotmail.com>
2023-07-16 18:48:05 -0300

    nvim: expand lualine config

--- config/nvim/lua/self/lualine.lua
+++ config/nvim/lua/self/lualine.lua
@@ -2,8 +2,23 @@
 require("lualine").setup({
   options = {
     theme = "catppuccin",
+    disabled_filetypes = { "neo-tree" },
+    globalstatus = true,
   },
   sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff", "diagnostics" },
     lualine_c = { "filename", "lsp_progress" },
+    lualine_x = { "encoding", "filetype" },
+    lualine_y = { "progress" },
+    lualine_z = { "location" },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
   },
 })
