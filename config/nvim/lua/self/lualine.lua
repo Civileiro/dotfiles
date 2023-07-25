9244115d85c4ddc3ed32e3b79bdad41ec331adb9
Gabriel Prost <gabrielprost@hotmail.com>
2023-07-25 10:22:47 -0300

    nvim: adjust status line

--- config/nvim/lua/self/lualine.lua
+++ config/nvim/lua/self/lualine.lua
@@ -7,18 +7,20 @@ require("lualine").setup({
   },
   sections = {
     lualine_a = { "mode" },
-    lualine_b = { "branch", "diff", "diagnostics" },
-    lualine_c = { "filename", "lsp_progress" },
-    lualine_x = { "encoding", "filetype" },
+    lualine_b = { "branch", "filename" },
+    lualine_c = { "diff", "diagnostics", "lsp_progress" },
+    lualine_x = {
+      {
+        "encoding",
+        cond = function()
+          local enconding = vim.opt.fileencoding:get()
+          if enconding == "utf-8" then return false end
+          return true
+        end
+      },
+      "filetype",
+    },
     lualine_y = { "progress" },
     lualine_z = { "location" },
-  },
-  inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { "filename" },
-    lualine_x = { "location" },
-    lualine_y = {},
-    lualine_z = {},
   },
 })
