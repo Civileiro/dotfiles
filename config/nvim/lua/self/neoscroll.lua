bb0fc9ae7687e1c03be4f8c02a84f9fe337cd934
Gabriel Prost <gabrielprost@hotmail.com>
2023-07-25 21:15:30 -0300

    nvim: smooth scroll in visual mode

--- config/nvim/lua/self/neoscroll.lua
+++ config/nvim/lua/self/neoscroll.lua
@@ -4,11 +4,11 @@ ns.setup({
   mappings = { "zz" }
 })
 
-vim.keymap.set("n", "<PageUp>", function ()
+vim.keymap.set({ "n", "v" }, "<PageUp>", function ()
   local h = vim.api.nvim_win_get_height(0)
   ns.scroll(-h, true, 200)
 end)
-vim.keymap.set("n", "<PageDown>", function ()
+vim.keymap.set({ "n", "v" }, "<PageDown>", function ()
   local h = vim.api.nvim_win_get_height(0)
   ns.scroll(h, true, 200)
 end)
