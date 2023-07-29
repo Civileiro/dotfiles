e91fcdb05c74d287a0b0a3c0601833761e112d7a
Gabriel Prost <gabrielprost@hotmail.com>
2023-07-28 21:32:47 -0300

    nvim: show hidden files

--- config/nvim/lua/self/neotree.lua
+++ config/nvim/lua/self/neotree.lua
@@ -1,9 +1,14 @@
 -- neotree.lua
 require("neo-tree").setup({
   close_if_last_window = true,
+  filesystem = {
+    filtered_items = {
+      visible = true,
+      hide_dotfiles = false,
+      hide_gitignore = true,
+    },
+  },
 })
 vim.keymap.set("n", "<Leader>e", "<Cmd>Neotree toggle<CR>", {
   desc = "Toggle [E]xplorer (neo-tree)"
 })
-
-
