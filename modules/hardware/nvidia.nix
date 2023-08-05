308de92dd38c8ab35e2e381621850079510f98f6
Gabriel Prost <gabrielprost@hotmail.com>
2023-08-05 14:13:07 -0300

    nvidia: add support for prime

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -1,36 +1,53 @@
 { config, lib, pkgs, ... }:
 with lib;
-let 
-  cfg = config.modules.hardware.nvidia;
+let cfg = config.modules.hardware.nvidia;
 in {
 
   options.modules.hardware.nvidia = {
-    enable = mkEnableOption "NVidia drivers";
-  };
-
-  config = mkIf cfg.enable {
-    hardware = {
-      nvidia = {
-        modesetting.enable = true;
-        forceFullCompositionPipeline = true;
-      };
-      opengl = {
-        enable = true;
-        driSupport = true;
-        driSupport32Bit = true;
-      };
+    enable = mkEnableOption "Nvidia drivers";
+    prime = {
+      enable = mkEnableOption "Nvidia Prime";
+      nvidiaBusId = my.mkOpt types.str "";
+      intelBusId = my.mkOpt types.str "";
     };
-
-    services.xserver.videoDrivers = [ "nvidia" ];
-    user.extraGroups = [ "video" ];
-
-    environment.systemPackages = with pkgs; [
-      # Respect XDG conventions, damn it!
-      (writeScriptBin "nvidia-settings" ''
-        #!${stdenv.shell}
-        mkdir -p "$XDG_CONFIG_HOME/nvidia"
-        exec ${config.boot.kernelPackages.nvidia_x11.settings}/bin/nvidia-settings --config="$XDG_CONFIG_HOME/nvidia/settings"
-      '')
-    ];
   };
+
+  config = mkIf cfg.enable (mkMerge [
+    {
+      hardware = {
+        nvidia = {
+          modesetting.enable = true;
+          forceFullCompositionPipeline = true;
+        };
+        opengl = {
+          enable = true;
+          driSupport = true;
+          driSupport32Bit = true;
+        };
+      };
+
+      services.xserver.videoDrivers = [ "nvidia" ];
+      user.extraGroups = [ "video" ];
+
+      environment.systemPackages = with pkgs;
+        [
+          # Respect XDG conventions, damn it!
+          (writeScriptBin "nvidia-settings" ''
+            #!${stdenv.shell}
+            mkdir -p "$XDG_CONFIG_HOME/nvidia"
+            exec ${config.boot.kernelPackages.nvidia_x11.settings}/bin/nvidia-settings --config="$XDG_CONFIG_HOME/nvidia/settings"
+          '')
+        ];
+    }
+    (mkIf cfg.prime.enable {
+      hardware.nvidia.prime = {
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+        intelBusId = cfg.prime.intelBusId;
+        nvidiaBusId = cfg.prime.nvidiaBusId;
+      };
+    })
+  ]);
 }
