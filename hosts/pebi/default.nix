7824e275702fb6d8f73a05f18ac622dc6c48efb3
Gabriel Prost <gabrielprost@hotmail.com>
2023-08-05 14:52:37 -0300

    new user: laptop pebi

---
+++ hosts/pebi/default.nix
@@ -0,0 +1,54 @@
+{ ... }: {
+  imports = [ (import ./hardware-configuration.nix) ];
+
+  # Configure console keymap
+  console.keyMap = "br-abnt2";
+
+  modules = {
+    shell = {
+      nu.enable = true;
+      git.enable = true;
+      tmux.enable = true;
+      utils = { btop.enable = true; };
+    };
+    editors = {
+      default = "nvim";
+      nvim.enable = true;
+    };
+    dev = {
+      xdg.enable = true;
+      rust.enable = true;
+      python.enable = true;
+      cc.enable = true;
+      nix.enable = true;
+    };
+    services = {
+      docker.enable = true;
+      networkmanager.enable = true;
+    };
+    desktop = {
+      plasma.enable = true;
+      apps = { discord.enable = true; };
+      browsers = {
+        default = "firefox";
+        firefox.enable = true;
+      };
+      gaming = { steam.enable = true; };
+      media = {
+        nomacs.enable = true;
+        vlc.enable = true;
+        libreoffice.enable = true;
+        kate.enable = true;
+      };
+      terminal = {
+        default = "alacritty";
+        alacritty = {
+          enable = true;
+          fontsize = 16;
+        };
+      };
+    };
+
+  };
+
+}
---
+++ hosts/pebi/hardware-configuration.nix
@@ -0,0 +1,51 @@
+{ config, lib, modulesPath, ... }:
+
+{
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+
+  boot.initrd.availableKernelModules =
+    [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  modules = {
+    hardware = {
+      nvidia = {
+        enable = true;
+        prime = {
+          enable = true;
+          intelBusId = "PCI:0:2:0";
+          nvidiaBusId = "PCI:1:0:0";
+        };
+      };
+      audio.enable = true;
+      fs.enable = true;
+    };
+  };
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-label/boot";
+    fsType = "vfat";
+  };
+
+  swapDevices = [{ device = "/dev/disk/by-label/swap"; }];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp62s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
