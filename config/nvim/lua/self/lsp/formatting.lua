9fa2d06224853b02d079b5b651e5af8050a76608
Gabriel Prost <gabrielprost@hotmail.com>
2023-08-09 23:12:33 -0300

    nvim: disable certain lsp formatters

--- config/nvim/lua/self/lsp/formatting.lua
+++ config/nvim/lua/self/lsp/formatting.lua
@@ -14,15 +14,24 @@ require("formatter").setup({
   }
 })
 
+local disabled_lsp_formatters = {
+  pyright = true,
+  ruff_lsp = true,
+}
+
 vim.keymap.set("n", "<C-s>", function()
   -- format with lsp if there's one capable
   local lsp_format = false
   for _, client in pairs(vim.lsp.buf_get_clients()) do
+    if disabled_lsp_formatters[client.name] then
+      goto continue
+    end
     if client.supports_method("textDocument/formatting") then
       vim.lsp.buf.format()
       lsp_format = true
       break
     end
+    ::continue::
   end
   -- fallback to external formatter
   if not lsp_format then
