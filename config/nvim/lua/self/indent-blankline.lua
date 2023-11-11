9ae74a0dd2028d1d40523aa0aef33bb3aacaff80
Gabriel Prost <gabrielprost@hotmail.com>
2023-11-11 13:20:19 -0300

    bump

--- config/nvim/lua/self/indent-blankline.lua
+++ config/nvim/lua/self/indent-blankline.lua
@@ -1,2 +1,2 @@
 -- indent-blankline.lua
-require("indent_blankline").setup({})
+require("ibl").setup({})
--- modules/desktop/gaming/emulators.nix
+++ modules/desktop/gaming/emulators.nix
@@ -3,18 +3,16 @@ with lib;
 let cfg = config.modules.desktop.gaming.emulators;
 in {
   options.modules.desktop.gaming.emulators = {
-    psx.enable = mkEnableOption "Playstation Emulator";
-    ds.enable = mkEnableOption "Nintendo DS Emulator";
+    # psx.enable = mkEnableOption "Playstation Emulator";
+    # ds.enable = mkEnableOption "Nintendo DS Emulator";
     gb.enable = mkEnableOption "GameBoy + GameBoy Color Emulator";
     gba.enable = mkEnableOption "GameBoy Advance Emulator";
-    snes.enable = mkEnableOption "Super Nintendo Emulator";
+    # snes.enable = mkEnableOption "Super Nintendo Emulator";
   };
 
   config = {
     user.packages = with pkgs; [
-      (mkIf cfg.psx.enable epsxe)
-      (mkIf cfg.ds.enable desmume)
-      (mkIf (cfg.gba.enable || cfg.gb.enable || cfg.snes.enable) higan)
+      (mkIf (cfg.gb.enable || cfg.gba.enable) mgba)
     ];
   };
 }
