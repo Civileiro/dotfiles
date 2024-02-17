fffa54b98a4b610f03b5b3c605dd47a019d34099
Gabriel Prost <gabrielprost@hotmail.com>
2024-02-16 23:13:03 -0300

    reformat transmission module & enable seeding

--- modules/services/transmission.nix
+++ modules/services/transmission.nix
@@ -10,19 +10,13 @@ in {
     services.transmission = {
       enable = true;
       # home = "${config.user.home}/torrents";
+      openFirewall = true;
       settings = {
         incomplete-dir-enabled = true;
         rpc-whitelist = "127.0.0.1,192.168.*.*";
         rpc-host-whitelist = "*";
         rpc-host-whitelist-enabled = true;
-        ratio-limit = 0;
-        ratio-limit-enabled = true;
       };
-    };
-
-    networking.firewall = {
-      allowedTCPPorts = [ 51413 ];
-      allowedUDPPorts = [ 51413 ];
     };
 
     user.extraGroups = [ "transmission" ];
