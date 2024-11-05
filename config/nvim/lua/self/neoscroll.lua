3d1dff57e4d65ecf8a4f35985a8e2faf94cb89fb
Gabriel Prost <gabrielprost@hotmail.com>
2024-11-05 16:53:16 -0300

    nvim: update neoscroll

--- config/nvim/lua/self/neoscroll.lua
+++ config/nvim/lua/self/neoscroll.lua
@@ -4,11 +4,11 @@ ns.setup({
   mappings = { "zz" }
 })
 
-vim.keymap.set({ "n", "v" }, "<PageUp>", function ()
+vim.keymap.set({ "n", "v" }, "<PageUp>", function()
   local h = vim.api.nvim_win_get_height(0)
-  ns.scroll(-h, true, 200)
+  ns.scroll(-h, { duration = 200 })
 end)
-vim.keymap.set({ "n", "v" }, "<PageDown>", function ()
+vim.keymap.set({ "n", "v" }, "<PageDown>", function()
   local h = vim.api.nvim_win_get_height(0)
-  ns.scroll(h, true, 200)
+  ns.scroll(h, { duration = 200 })
 end)
