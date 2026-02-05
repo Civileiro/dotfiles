9ff6d16b68d1843c14ced081591b0e589f0b5f10
Gabriel Prost <gabrielprost@hotmail.com>
2026-02-05 17:27:29 -0300

    bump
    updated treesitter

--- config/nvim/lua/self/treesitter.lua
+++ config/nvim/lua/self/treesitter.lua
@@ -1,13 +1,19 @@
 -- treesitter.lua
-require("nvim-treesitter.configs").setup({
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      node_incremental = "<C-Up>",
-      node_decremental = "<C-Down>",
-    },
-  },
-  highlight = {
-    enable = true,
-  },
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("tree-sitter-enable", { clear = true }),
+  callback = function(args)
+    local lang = vim.treesitter.language.get_lang(args.match)
+    if not lang then return end
+
+    if vim.treesitter.query.get(lang, "highlights") then vim.treesitter.start(args.buf) end
+
+    if vim.treesitter.query.get(lang, "indents") then
+      vim.opt_local.indentexpr = 'v:lua.require("nvim-treesitter").indentexpr()'
+    end
+
+    if vim.treesitter.query.get(lang, "folds") then
+      vim.opt_local.foldmethod = "expr"
+      vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+    end
+  end,
 })
